ac399936e82e6048515fa8d2146b4cf3c71c36e6
alexotooro <kakajoon63@gmail.com>
2025-10-31 01:22:07 +0330

    Update TransactionAdapter.kt

--- app/src/main/java/org/hesab/app/TransactionAdapter.kt
+++ app/src/main/java/org/hesab/app/TransactionAdapter.kt
@@ -1,59 +1,43 @@
-package org.hesab.app
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content"
+    android:orientation="horizontal"
+    android:padding="8dp"
+    android:gravity="center_vertical">
 
-import android.view.LayoutInflater
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.PopupMenu
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
+    <TextView
+        android:id="@+id/txt_date"
+        android:layout_width="0dp"
+        android:layout_height="wrap_content"
+        android:layout_weight="1"
+        android:text="1403/07/01" />
 
-class TransactionAdapter(
-    private val transactions: MutableList<Transaction>,
-    private val listener: OnTransactionMenuClickListener
-) : RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder>() {
+    <TextView
+        android:id="@+id/txt_amount"
+        android:layout_width="0dp"
+        android:layout_height="wrap_content"
+        android:layout_weight="1"
+        android:text="50000" />
 
-    interface OnTransactionMenuClickListener {
-        fun onEditClicked(transaction: Transaction)
-        fun onDeleteClicked(transaction: Transaction)
-    }
+    <TextView
+        android:id="@+id/txt_reason"
+        android:layout_width="0dp"
+        android:layout_height="wrap_content"
+        android:layout_weight="1"
+        android:text="خوراک" />
 
-    inner class TransactionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val txtDate: TextView = view.findViewById(R.id.txt_date)
-        val txtAmount: TextView = view.findViewById(R.id.txt_amount)
-        val txtReason: TextView = view.findViewById(R.id.txt_reason)
-        val txtNote: TextView = view.findViewById(R.id.txt_note)
-        val menuIcon: ImageView = view.findViewById(R.id.menu_icon)
-    }
+    <TextView
+        android:id="@+id/txt_note"
+        android:layout_width="0dp"
+        android:layout_height="wrap_content"
+        android:layout_weight="1"
+        android:text="-" />
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_transaction, parent, false)
-        return TransactionViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: TransactionViewHolder, position: Int) {
-        val transaction = transactions[position]
-        holder.txtDate.text = transaction.date
-        holder.txtAmount.text = transaction.amount.toString()
-        holder.txtReason.text = transaction.reason
-        holder.txtNote.text = transaction.note
-
-        holder.menuIcon.setOnClickListener {
-            val popup = PopupMenu(holder.itemView.context, holder.menuIcon)
-            MenuInflater(holder.itemView.context).inflate(R.menu.menu_transaction_item, popup.menu)
-            popup.setOnMenuItemClickListener { item: MenuItem ->
-                when (item.itemId) {
-                    R.id.menu_edit -> listener.onEditClicked(transaction)
-                    R.id.menu_delete -> listener.onDeleteClicked(transaction)
-                }
-                true
-            }
-            popup.show()
-        }
-    }
-
-    override fun getItemCount(): Int = transactions.size
-}
+    <ImageView
+        android:id="@+id/menu_icon"
+        android:layout_width="24dp"
+        android:layout_height="24dp"
+        android:src="@drawable/ic_more_vert"
+        android:contentDescription="menu" />
+</LinearLayout>
