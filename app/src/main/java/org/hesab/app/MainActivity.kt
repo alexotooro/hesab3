e8f97ebcd3bbc18677ea5c2a8810da51318f582f
alexotooro <kakajoon63@gmail.com>
2025-10-30 13:20:45 +0330

    Update MainActivity.kt

--- app/src/main/java/org/hesab/app/MainActivity.kt
+++ app/src/main/java/org/hesab/app/MainActivity.kt
@@ -1,6 +1,5 @@
 package org.hesab.app
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,7 +10,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var db: AppDatabase
     private lateinit var adapter: TransactionAdapter
-    private lateinit var layoutManager: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,38 +18,38 @@ class MainActivity : AppCompatActivity() {
 
         db = AppDatabase.getInstance(this)
 
-        setupRecyclerView()
-        loadTransactions()
+        // تنظیم RecyclerView
+        adapter = TransactionAdapter()
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
 
+        // کلیک دکمه +
         binding.fabAdd.setOnClickListener {
-            val intent = Intent(this, AddTransactionActivity::class.java)
-            startActivity(intent)
+            startActivity(android.content.Intent(this, AddTransactionActivity::class.java))
         }
     }
 
     override fun onResume() {
         super.onResume()
-        loadTransactions()
+        loadTransactionsAndBalance()
     }
 
-    private fun setupRecyclerView() {
-        layoutManager = LinearLayoutManager(this)
-        // قدیمی‌ترها بالا، جدیدترها پایین
-        layoutManager.reverseLayout = false
-        layoutManager.stackFromEnd = true  // اسکرول خودکار به پایین
-        binding.recyclerView.layoutManager = layoutManager
-
-        adapter = TransactionAdapter()
-        binding.recyclerView.adapter = adapter
-    }
-
-    private fun loadTransactions() {
+    private fun loadTransactionsAndBalance() {
         val transactions = db.transactionDao().getAll()
-        adapter.setData(transactions)
+        adapter.submitList(transactions)
 
-        // اسکرول خودکار به آخرین آیتم (پایین لیست)
-        if (transactions.isNotEmpty()) {
-            binding.recyclerView.scrollToPosition(transactions.size - 1)
+        // محاسبه مانده
+        var incomeTotal = 0.0
+        var expenseTotal = 0.0
+
+        for (t in transactions) {
+            if (t.type == "درآمد") incomeTotal += t.amount
+            else if (t.type == "هزینه") expenseTotal += t.amount
         }
+
+        val balance = incomeTotal - expenseTotal
+        val balanceText = "مانده: %, .0f ریال".format(balance)
+
+        binding.tvBalance.text = balanceText
     }
 }
