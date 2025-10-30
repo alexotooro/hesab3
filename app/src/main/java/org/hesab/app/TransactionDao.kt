94d1959bea1b9adbceaa853d2c94102fed3b7b4a
alexotooro <kakajoon63@gmail.com>
2025-10-30 15:32:35 +0330

    Update TransactionDao.kt

--- app/src/main/java/org/hesab/app/TransactionDao.kt
+++ app/src/main/java/org/hesab/app/TransactionDao.kt
@@ -3,6 +3,8 @@ package org.hesab.app
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
+import androidx.room.Delete
 
 @Dao
 interface TransactionDao {
@@ -10,6 +12,15 @@ interface TransactionDao {
     @Insert
     fun insert(transaction: Transaction)
 
+    @Update
+    fun update(transaction: Transaction)
+
+    @Delete
+    fun delete(transaction: Transaction)
+
     @Query("SELECT * FROM transactions ORDER BY id DESC")
     fun getAll(): List<Transaction>
+
+    @Query("SELECT * FROM transactions WHERE id = :id LIMIT 1")
+    fun getById(id: Int): Transaction?
 }
