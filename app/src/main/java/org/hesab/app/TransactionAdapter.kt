8b656f5f89efa9cc9ca12cda6f516677864754de
alexotooro <kakajoon63@gmail.com>
2025-10-31 01:16:24 +0330

    Update TransactionAdapter.kt

--- app/src/main/java/org/hesab/app/TransactionAdapter.kt
+++ app/src/main/java/org/hesab/app/TransactionAdapter.kt
@@ -1,21 +1,31 @@
 package org.hesab.app
 
 import android.view.LayoutInflater
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class TransactionAdapter(
-    private var transactions: MutableList<Transaction>,
+    private val transactions: MutableList<Transaction>,
     private val listener: OnTransactionMenuClickListener
 ) : RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder>() {
 
     interface OnTransactionMenuClickListener {
-        fun onEditClick(transaction: Transaction)
-        fun onDeleteClick(transaction: Transaction)
+        fun onEditClicked(transaction: Transaction)
+        fun onDeleteClicked(transaction: Transaction)
+    }
+
+    inner class TransactionViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val txtDate: TextView = view.findViewById(R.id.txtDate)
+        val txtAmount: TextView = view.findViewById(R.id.txtAmount)
+        val txtReason: TextView = view.findViewById(R.id.txtReason)
+        val txtNote: TextView = view.findViewById(R.id.txtNote)
+        val menuIcon: ImageView = view.findViewById(R.id.menuIcon)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionViewHolder {
@@ -26,45 +36,24 @@ class TransactionAdapter(
 
     override fun onBindViewHolder(holder: TransactionViewHolder, position: Int) {
         val transaction = transactions[position]
+        holder.txtDate.text = transaction.date
+        holder.txtAmount.text = transaction.amount.toString()
+        holder.txtReason.text = transaction.reason
+        holder.txtNote.text = transaction.note
 
-        holder.tvDate.text = transaction.date
-        holder.tvAmount.text = transaction.amount.toString()
-        holder.tvCategory.text = transaction.category
-        holder.tvDescription.text = transaction.description
-
-        holder.btnMenu.setOnClickListener {
-            val popup = PopupMenu(holder.itemView.context, holder.btnMenu)
-            popup.inflate(R.menu.transaction_item_menu)
-            popup.setOnMenuItemClickListener {
-                when (it.itemId) {
-                    R.id.menu_edit -> {
-                        listener.onEditClick(transaction)
-                        true
-                    }
-                    R.id.menu_delete -> {
-                        listener.onDeleteClick(transaction)
-                        true
-                    }
-                    else -> false
+        holder.menuIcon.setOnClickListener {
+            val popup = PopupMenu(holder.itemView.context, holder.menuIcon)
+            MenuInflater(holder.itemView.context).inflate(R.menu.transaction_item_menu, popup.menu)
+            popup.setOnMenuItemClickListener { item: MenuItem ->
+                when (item.itemId) {
+                    R.id.menu_edit -> listener.onEditClicked(transaction)
+                    R.id.menu_delete -> listener.onDeleteClicked(transaction)
                 }
+                true
             }
             popup.show()
         }
     }
 
     override fun getItemCount(): Int = transactions.size
-
-    fun updateData(newList: List<Transaction>) {
-        transactions.clear()
-        transactions.addAll(newList)
-        notifyDataSetChanged()
-    }
-
-    inner class TransactionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvDate: TextView = itemView.findViewById(R.id.tvDate)
-        val tvAmount: TextView = itemView.findViewById(R.id.tvAmount)
-        val tvCategory: TextView = itemView.findViewById(R.id.tvCategory)
-        val tvDescription: TextView = itemView.findViewById(R.id.tvDescription)
-        val btnMenu: ImageButton = itemView.findViewById(R.id.btnMenu)
-    }
 }
