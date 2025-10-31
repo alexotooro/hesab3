e0bcdc1e62f3836d66e9b0ce2fe7f5382cf9bfb8
alexotooro <kakajoon63@gmail.com>
2025-10-31 05:29:32 +0330

    Create App.kt

---
+++ app/src/main/java/org/hesab/app/App.kt
@@ -0,0 +1,21 @@
+package org.hesab.app
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "transaction_channel",
+                "تراکنش‌ها",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(channel)
+        }
+    }
+}
