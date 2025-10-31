a9f3f1e79213453c69b4611a065d87a45debeb68
alexotooro <kakajoon63@gmail.com>
2025-10-31 05:02:24 +0330

    Update AppDatabase.kt

--- app/src/main/java/org/hesab/app/AppDatabase.kt
+++ app/src/main/java/org/hesab/app/AppDatabase.kt
@@ -5,23 +5,22 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Transaction::class], version = 1)
+@Database(entities = [Transaction::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun transactionDao(): TransactionDao
 
     companion object {
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
+        @Volatile private var INSTANCE: AppDatabase? = null
 
         fun getInstance(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
+                val inst = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "transactions_db"
-                ).build()
-                INSTANCE = instance
-                instance
+                    "hesab_db"
+                ).fallbackToDestructiveMigration().build()
+                INSTANCE = inst
+                inst
             }
         }
     }
