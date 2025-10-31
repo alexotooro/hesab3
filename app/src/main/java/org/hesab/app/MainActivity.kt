f4aeb8deeddf11d7dbe8feb7816ccab6d69a0f67
alexotooro <kakajoon63@gmail.com>
2025-10-31 07:07:06 +0330

    Update MainActivity.kt

--- app/src/main/java/org/hesab/app/MainActivity.kt
+++ app/src/main/java/org/hesab/app/MainActivity.kt
@@ -2,11 +2,17 @@ package org.hesab.app
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.*
-import kotlinx.coroutines.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: TransactionAdapter
 
@@ -16,30 +22,45 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = TransactionAdapter(mutableListOf())
+        recyclerView.adapter = adapter
 
-        loadTransactions()
-
-        findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.fabAdd).setOnClickListener {
-            startActivity(Intent(this, AddTransactionActivity::class.java))
-        }
-
-        findViewById<android.widget.ImageButton>(R.id.btnSettings).setOnClickListener {
-            startActivity(Intent(this, SettingsActivity::class.java))
-        }
-    }
-
-    override fun onResume() {
-        super.onResume()
         loadTransactions()
     }
 
     private fun loadTransactions() {
         CoroutineScope(Dispatchers.IO).launch {
-            val transactions = App.db.transactionDao().getAll()
-            withContext(Dispatchers.Main) {
-                adapter = TransactionAdapter(transactions.toMutableList(), this@MainActivity)
-                recyclerView.adapter = adapter
+            val db = AppDatabase.getDatabase(this@MainActivity)
+            val prefs = getSharedPreferences("org.hesab.app_preferences", MODE_PRIVATE)
+            val sortDescending = prefs.getBoolean("sort_descending", false)
+
+            val transactions = db.transactionDao().getAll().let {
+                if (sortDescending) it.reversed() else it
+            }
+
+            runOnUiThread {
+                adapter.setData(transactions)
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_settings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadTransactions()
     }
 }
