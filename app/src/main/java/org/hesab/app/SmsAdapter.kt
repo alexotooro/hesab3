b9f94c279a6e8340bf7591f99d51708a568f45dc
alexotooro <kakajoon63@gmail.com>
2025-10-31 12:54:33 +0330

    Update SmsAdapter.kt

--- app/src/main/java/org/hesab/app/SmsAdapter.kt
+++ app/src/main/java/org/hesab/app/SmsAdapter.kt
@@ -6,27 +6,32 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class SmsAdapter(private val smsList: List<Transaction>) :
+class SmsAdapter(private val transactions: List<Transaction>) :
     RecyclerView.Adapter<SmsAdapter.SmsViewHolder>() {
 
-    class SmsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val tvDate: TextView = view.findViewById(R.id.tvSmsDate)
-        val tvAmount: TextView = view.findViewById(R.id.tvSmsAmount)
-        val tvType: TextView = view.findViewById(R.id.tvSmsType)
+    class SmsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvDate: TextView = itemView.findViewById(R.id.tvSmsDate)
+        val tvAmount: TextView = itemView.findViewById(R.id.tvSmsAmount)
+        val tvType: TextView = itemView.findViewById(R.id.tvSmsType)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SmsViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_sms, parent, false)
+            .inflate(R.layout.item_sms_transaction, parent, false)
         return SmsViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: SmsViewHolder, position: Int) {
-        val sms = smsList[position]
-        holder.tvDate.text = sms.date
-        holder.tvAmount.text = "${sms.amount} ریال"
-        holder.tvType.text = if (sms.isIncome) "واریز" else "برداشت"
+        val transaction = transactions[position]
+        holder.tvDate.text = transaction.date
+        holder.tvAmount.text = "%,d ریال".format(transaction.amount)
+        holder.tvType.text = if (transaction.isIncome) "واریز" else "برداشت"
+        holder.tvType.setTextColor(
+            holder.itemView.context.getColor(
+                if (transaction.isIncome) R.color.income_green else R.color.expense_red
+            )
+        )
     }
 
-    override fun getItemCount(): Int = smsList.size
+    override fun getItemCount(): Int = transactions.size
 }
