d30ae4e19e1d1c7ce6a75a9eb64645a6ebe51e3d
alexotooro <kakajoon63@gmail.com>
2025-10-31 06:49:28 +0330

    Update TransactionDao.kt

--- app/src/main/java/org/hesab/app/TransactionDao.kt
+++ app/src/main/java/org/hesab/app/TransactionDao.kt
@@ -4,20 +4,15 @@ import androidx.room.*
 
 @Dao
 interface TransactionDao {
-
-    // مرتب بر اساس id DESC => جدیدترین بالاست (ثبت‌شده‌ترین)
-    @Query("SELECT * FROM transactions ORDER BY id DESC")
+    @Query("SELECT * FROM transactions ORDER BY orderIndex ASC")
     fun getAll(): List<Transaction>
 
-    @Query("SELECT * FROM transactions WHERE id = :id LIMIT 1")
-    fun getById(id: Long): Transaction?
-
     @Insert
-    fun insert(transaction: Transaction): Long
-
-    @Update
-    fun update(transaction: Transaction)
+    fun insert(transaction: Transaction)
 
     @Delete
     fun delete(transaction: Transaction)
+
+    @Update
+    fun update(transaction: Transaction)
 }
