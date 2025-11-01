5ce8264145ae0a6902f38cce627161f729895bbb
alexotooro <kakajoon63@gmail.com>
2025-11-02 03:21:04 +0330

    Update AppDatabase.kt

--- app/src/main/java/org/hesab/app/AppDatabase.kt
+++ app/src/main/java/org/hesab/app/AppDatabase.kt
@@ -4,32 +4,22 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-import org.hesab.app.utils.Converters   // 🔹 این خط خیلی مهمه
 
-@Database(
-    entities = [Transaction::class],
-    version = 1,
-    exportSchema = false
-)
-@TypeConverters(Converters::class)
+@Database(entities = [Transaction::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
-
     abstract fun transactionDao(): TransactionDao
 
     companion object {
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
-        fun getDatabase(context: Context): AppDatabase {
+        fun getInstance(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "hesab_db"
-                )
-                    .fallbackToDestructiveMigration()
-                    .build()
+                    "transactions_db"
+                ).build()
                 INSTANCE = instance
                 instance
             }
