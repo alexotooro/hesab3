ad5f584d7c484d84f024fb1234f2da7ca5ceeaf8
alexotooro <kakajoon63@gmail.com>
2025-11-02 01:48:29 +0330

    Update SmsAdapter.kt

--- app/src/main/java/org/hesab/app/SmsAdapter.kt
+++ app/src/main/java/org/hesab/app/SmsAdapter.kt
@@ -6,32 +6,27 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class SmsAdapter(private val transactions: List<Transaction>) :
+class SmsAdapter(private val smsList: List<Transaction>) :
     RecyclerView.Adapter<SmsAdapter.SmsViewHolder>() {
 
-    class SmsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvDate: TextView = itemView.findViewById(R.id.tvSmsDate)
-        val tvAmount: TextView = itemView.findViewById(R.id.tvSmsAmount)
-        val tvType: TextView = itemView.findViewById(R.id.tvSmsType)
+    class SmsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val tvAmount: TextView = view.findViewById(R.id.tvAmount)
+        val tvCategory: TextView = view.findViewById(R.id.tvCategory)
+        val tvNote: TextView = view.findViewById(R.id.tvNote)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SmsViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_sms_transaction, parent, false)
+            .inflate(R.layout.item_transaction, parent, false)
         return SmsViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: SmsViewHolder, position: Int) {
-        val transaction = transactions[position]
-        holder.tvDate.text = transaction.date
-        holder.tvAmount.text = "%,d ریال".format(transaction.amount)
-        holder.tvType.text = if (transaction.isIncome) "واریز" else "برداشت"
-        holder.tvType.setTextColor(
-            holder.itemView.context.getColor(
-                if (transaction.isIncome) R.color.income_green else R.color.expense_red
-            )
-        )
+        val sms = smsList[position]
+        holder.tvAmount.text = sms.amount.toString()
+        holder.tvCategory.text = sms.category
+        holder.tvNote.text = sms.note ?: ""
     }
 
-    override fun getItemCount(): Int = transactions.size
+    override fun getItemCount(): Int = smsList.size
 }
