9c590438cf5e11e58b50d7b84a6ea0ed44a3d79d
alexotooro <kakajoon63@gmail.com>
2025-11-02 01:46:56 +0330

    Update Transaction.kt

--- app/src/main/java/org/hesab/app/Transaction.kt
+++ app/src/main/java/org/hesab/app/Transaction.kt
@@ -8,10 +8,10 @@ data class Transaction(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
 
-    val date: Long = System.currentTimeMillis(),  // ✅ زمان به صورت timestamp
-    val amount: Long,                             // مبلغ
-    val category: String,                         // بابت (مثل خرید یا واریز)
-    val note: String? = null,                     // توضیحات
-    val isIncome: Boolean = false,                // درآمد یا هزینه
-    val orderIndex: Int = 0                       // ترتیب در RecyclerView
+    val date: Long = System.currentTimeMillis(),   // تاریخ به صورت timestamp
+    val amount: Long,                              // مبلغ
+    val category: String,                          // بابت
+    val note: String? = null,                      // توضیحات
+    val isIncome: Boolean = false,                 // درآمد یا هزینه
+    val orderIndex: Int = 0                        // ترتیب نمایش
 )
