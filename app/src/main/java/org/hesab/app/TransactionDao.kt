6b4f80bc5e6a01756bcbf42cc86027c448ea3643
alexotooro <kakajoon63@gmail.com>
2025-11-02 01:47:21 +0330

    Update TransactionDao.kt

--- app/src/main/java/org/hesab/app/TransactionDao.kt
+++ app/src/main/java/org/hesab/app/TransactionDao.kt
@@ -4,15 +4,22 @@ import androidx.room.*
 
 @Dao
 interface TransactionDao {
+
     @Query("SELECT * FROM transactions ORDER BY orderIndex ASC")
     fun getAll(): List<Transaction>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(transaction: Transaction)
+
+    @Update
+    fun update(transaction: Transaction)
 
     @Delete
     fun delete(transaction: Transaction)
 
+    @Query("UPDATE transactions SET orderIndex = :newIndex WHERE id = :id")
+    fun updateOrder(id: Int, newIndex: Int)
+
     @Update
-    fun update(transaction: Transaction)
+    fun updateAll(transactions: List<Transaction>)
 }
